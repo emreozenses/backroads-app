403141060d6e8489320cb4cf4ba65ae7363a4d2f
Emre OZENSES <emreozenses@gmail.com>
2024-04-24 10:19:20 +0300

    SocialLinks component completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,15 @@
 import React from 'react'
 import {socialLinks } from '../data'
 import PageLinks from './PageLinks'
+import SocialLinks from './SocialLinks'
 
 const Footer = () => {
   return (
     <footer className="section footer">
-      <PageLinks parentClass='footer-links' itemClass='footer-link'/>
-      <ul className="footer-icons">
-        {socialLinks.map((link)=>{
-            const {id,href,icon}=link
-            return (
-              <li key={id}>
-                <a
-                  href={href}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="footer-icon"
-                >
-                  <i className={icon}></i>
-                </a>
-              </li>
-            )
-        })}
-              
-      </ul>
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+      <SocialLinks parentClass="footer-icons" itemClass="footer-icon" />
       <p className="copyright">
-        copyright &copy; Backroads travel tours company 
+        copyright &copy; Backroads travel tours company
         <span id="date">{new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo from '../images/logo.svg'
-import { socialLinks } from '../data'
 import PageLinks from './PageLinks'
+import SocialLinks from './SocialLinks'
 
 const Navbar = () => {
   return (
@@ -14,22 +14,7 @@ const Navbar = () => {
           </button>
         </div>
         <PageLinks parentClass='nav-links' itemClass='nav-link' />
-        <ul className="nav-icons">
-          {socialLinks.map((link)=>{
-            const {id,href,icon} = link
-            return(
-            <li key={id}>
-              <a
-                href={href}
-                target="_blank"
-                rel="noreferrer"
-                className="nav-icon"
-              >
-                <i className={icon}></i>
-              </a>
-            </li>
-          )})}
-        </ul>
+        <SocialLinks parentClass='nav-icons' itemClass='nav-icon'/>
       </div>
     </nav>
   )
